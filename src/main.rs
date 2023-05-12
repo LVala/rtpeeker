8dfb708c065af43e6fcf602581a867b49d69b3a2
LVala <84684231+LVala@users.noreply.github.com>
2023-05-12 22:02:07 +0200

    Finish rtp parsing function

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::path::Path;
 
 pub mod rtp_sniffer;
 
-
 fn main() {
-    let file = Path::new("./pcap_examples/sip_rtp.pcap");
-    rtp_sniffer::rtp_from_file(file);
+    let file = Path::new("./pcap_examples/rtp.pcap");
+    let packets = rtp_sniffer::rtp_from_file(file);
+    for packet in packets {
+        println!("{:?}", packet);
+    }
 }
--- src/rtp_sniffer.rs
+++ src/rtp_sniffer.rs
@@ -1,18 +1,33 @@
+use etherparse::{IpHeader, PacketHeaders, TransportHeader::Udp, UdpHeader};
+use pcap::Capture;
+use rtp::packet::Packet;
 use std::path::Path;
 use webrtc_util::marshal::Unmarshal;
-use etherparse::{PacketHeaders, TransportHeader::Udp};
-use rtp::packet::Packet;
-use pcap::Capture;
 
-pub fn rtp_from_file(file_name: &Path) {
+#[derive(Debug)]
+pub struct RtpPacket {
+    pub ip_header: IpHeader,
+    pub udp_header: UdpHeader,
+    pub rtp_packet: Packet,
+}
+
+pub fn rtp_from_file(file_name: &Path) -> Vec<RtpPacket> {
     let mut cap = Capture::from_file(file_name).unwrap();
+    let mut valid_packets: Vec<RtpPacket> = Vec::new();
 
-    while let Ok(packet) = cap.next_packet() {
-        let mut packet_headers = PacketHeaders::from_ethernet_slice(packet.data).unwrap();
-        if let Some(Udp(_header)) = packet_headers.transport {
-            let rtp_packet = Packet::unmarshal(&mut packet_headers.payload);
-            println!("{:?}", rtp_packet);
+    while let Ok(raw_packet) = cap.next_packet() {
+        let mut packet = PacketHeaders::from_ethernet_slice(raw_packet.data).unwrap();
+        if let Some(Udp(udp_header)) = packet.transport {
+            if let Ok(rtp_packet) = Packet::unmarshal(&mut packet.payload) {
+                let packet = RtpPacket {
+                    ip_header: packet.ip.unwrap(),
+                    udp_header,
+                    rtp_packet,
+                };
+                valid_packets.push(packet);
+            }
         }
-
     }
+
+    valid_packets
 }
