da5f53ad87c7fe8e2610be3cbd0256b7667d138d
Łukasz Wala <84684231+LVala@users.noreply.github.com>
2023-05-20 13:23:17 +0200

    Create struct that agregates stream informations (#4)

---
+++ src/analysis/rtp.rs
@@ -0,0 +1,65 @@
+use crate::rtp_sniffer::RtpPacket;
+use std::{net::SocketAddr, time::Duration};
+
+#[derive(Debug)]
+pub struct Stream<'a> {
+    pub source_addr: SocketAddr,
+    pub destination_addr: SocketAddr,
+    pub ssrc: u32,
+    // start time?
+    pub duration: Duration,
+    pub payload_type: u8,
+    // delta, jitter
+    lost_packets: usize,
+    packets: Vec<&'a RtpPacket>,
+}
+
+impl<'a> Stream<'a> {
+    pub fn new(packet: &'a RtpPacket) -> Self {
+        Self {
+            source_addr: packet.source_addr.clone(),
+            destination_addr: packet.destination_addr.clone(),
+            ssrc: packet.rtp_header.ssrc,
+            duration: Duration::new(0, 0),
+            payload_type: packet.rtp_header.payload_type,
+            lost_packets: 0,
+            packets: vec![packet],
+        }
+    }
+
+    pub fn add_packet(&mut self, packet: &'a RtpPacket) {
+        self.packets.push(packet);
+    }
+
+    pub fn num_of_packets(&self) -> usize {
+        self.packets.len()
+    }
+
+    pub fn lost_packets_percentage(&self) -> usize {
+        self.lost_packets / self.packets.len()
+    }
+}
+
+#[derive(Debug)]
+pub struct Streams<'a> {
+    pub streams: Vec<Stream<'a>>,
+}
+
+impl<'a> Streams<'a> {
+    pub fn new() -> Self {
+        Streams {
+            streams: Vec::new(),
+        }
+    }
+
+    pub fn add_packet(&mut self, packet: &'a RtpPacket) {
+        for stream in self.streams.iter_mut() {
+            if stream.ssrc == packet.rtp_header.ssrc {
+                stream.add_packet(packet);
+                return;
+            }
+        }
+        let new_stream = Stream::new(packet);
+        self.streams.push(new_stream);
+    }
+}
