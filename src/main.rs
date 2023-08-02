4c83f6de52c57950f53a9b639bc394057ca959d9
Łukasz Wala <84684231+LVala@users.noreply.github.com>
2023-08-02 22:40:50 +0200

    Setup warp and basic HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    warp::serve(warp::fs::dir("client/dist"))
+        .run(([127, 0, 0, 1], 3550))
+        .await;
 }
