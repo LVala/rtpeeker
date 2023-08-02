88863c2b74ce04d26238e8363d20e0c126a4a4fa
Łukasz Wala <84684231+LVala@users.noreply.github.com>
2023-08-02 22:34:54 +0200

    Setup egui in `client` sub-packege

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,19 @@
+pub mod app;
+
 fn main() {
-    println!("Hello, world!");
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(app::TemplateApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
