57cb56e400f0ea6ca15c1e610b58ad869e262ef4
damian-tworek <damian.tworek@spoton.com>
2023-08-13 19:11:05 +0200

    Move towards struct, wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod cmd;
 pub mod command_line_interface;
 pub mod sniffer;
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
-use rtpeeker::command_line_interface::start_interface::command_line_interface;
-use rtpeeker::command_line_interface::start_interface::Action::{AnalyzeFile, CapturePackets};
-use rtpeeker::sniffer;
 use std::net::SocketAddr;
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand};
+
+// use rtpeeker::command_line_interface::start_interface::command_line_interface;
+// use rtpeeker::command_line_interface::start_interface::Action::{AnalyzeFile, CapturePackets};
+use rtpeeker::{cmd, sniffer};
 
 #[tokio::main]
 async fn main() {
-    let action = command_line_interface();
-    match action {
-        CapturePackets(device, socket_addr) => {
-            capture_packets(device);
-            warp_serve(socket_addr).await
-        }
-        AnalyzeFile(path, socket_addr) => {
-            analyze_file(path);
-            warp_serve(socket_addr).await
-        }
-    };
+    let x = RtpeekerArgs::parse();
+    println!("{:?}", x);
+    // let action = command_line_interface();
+    // match action {
+    //     CapturePackets(device, socket_addr) => {
+    //         capture_packets(device);
+    //         warp_serve(socket_addr).await
+    //     }
+    //     AnalyzeFile(path, socket_addr) => {
+    //         analyze_file(path);
+    //         warp_serve(socket_addr).await
+    //     }
+    // };
+}
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct RtpeekerArgs {
+    #[clap(subcommand)]
+    action: CommandType,
+}
+
+#[derive(Debug, Subcommand)]
+enum CommandType {
+    File(FileSubcommand),
+    Interface(InterfaceSubcommand),
+    ListInterfaces(ListInterfaceSubcommand),
+}
+
+#[derive(Debug, Args)]
+struct FileSubcommand {
+    file_path: String,
+    socket_to_serve: String,
+}
+
+#[derive(Debug, Args)]
+struct InterfaceSubcommand {
+    interface_name: String,
+    socket_to_serve: String,
+}
+
+#[derive(Debug, Args)]
+struct ListInterfaceSubcommand {
+    filter_up: Option<bool>,
+    filter_loopback: Option<bool>,
+    filter_running: Option<bool>,
+    filter_wireless: Option<bool>,
 }
 
 async fn warp_serve(socket_addr: SocketAddr) {
