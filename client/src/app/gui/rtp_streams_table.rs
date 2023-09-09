45cf736203babd1eb2a04737d4679d4d003e7225
damian-tworek <damian.tworek@spoton.com>
2023-09-09 18:36:28 +0200

    Jitter

--- client/src/app/gui/rtp_streams_table.rs
+++ client/src/app/gui/rtp_streams_table.rs
@@ -1,7 +1,12 @@
-use super::Packets;
+use std::collections::HashSet;
+use std::time::Duration;
+
 use egui_extras::{Column, TableBody, TableBuilder};
-use rtpeeker_common::packet::{Packet, SessionProtocol};
-use rtpeeker_common::Request;
+use rtpeeker_common::packet::{Packet, SessionPacket};
+use rtpeeker_common::packet::SessionProtocol::Rtp;
+use rtpeeker_common::RtpPacket;
+
+use super::Packets;
 
 pub struct RtpStreamsTable {
     packets: Packets,
@@ -9,7 +14,9 @@ pub struct RtpStreamsTable {
 
 impl RtpStreamsTable {
     pub fn new(packets: Packets) -> Self {
-        Self { packets }
+        Self {
+            packets,
+        }
     }
 
     pub fn ui(&mut self, ctx: &egui::Context) {
@@ -20,23 +27,25 @@ impl RtpStreamsTable {
 
     fn build_table(&mut self, ui: &mut egui::Ui) {
         let header_labels = [
-            ("No.", "Packet number (including skipped packets)"),
-            ("Time", "Packet arrival timestamp"),
+            ("SSRC", "RTP SSRC (Synchronization Source Identifier) identifies the source of an RTP stream"),
             ("Source", "Source IP address and port"),
             ("Destination", "Destination IP address and port"),
-            ("Protocol", "Transport layer protocol"),
-            ("Length", "Length of the packet (including IP header)"),
-            ("Treated as", "How was the UDP/TCP payload parsed"),
+            ("Number of packets", "Number of packets in stream"),
+            ("Duration", "Difference between last timestamp and first timestamp."),
+            ("Lost packets", "Difference between last timestamp and first timestamp."),
+            ("Jitter", ""),
         ];
         TableBuilder::new(ui)
             .striped(true)
             .resizable(true)
             .stick_to_bottom(true)
             .column(Column::remainder().at_least(40.0))
-            .column(Column::remainder().at_least(130.0))
-            .columns(Column::remainder().at_least(100.0), 2)
-            .columns(Column::remainder().at_least(80.0), 2)
-            .column(Column::remainder().at_least(100.0))
+            .column(Column::remainder().at_least(40.0))
+            .column(Column::remainder().at_least(40.0))
+            .column(Column::remainder().at_least(40.0))
+            .column(Column::remainder().at_least(40.0))
+            .column(Column::remainder().at_least(40.0))
+            .column(Column::remainder().at_least(40.0))
             .header(30.0, |mut header| {
                 header_labels.iter().for_each(|(label, desc)| {
                     header.col(|ui| {
@@ -51,55 +60,139 @@ impl RtpStreamsTable {
     }
 
     fn build_table_body(&mut self, body: TableBody) {
-        let mut requests = Vec::new();
         let packets = self.packets.borrow();
+        let mut set: HashSet<u32> = HashSet::new();
 
-        body.rows(25.0, packets.len(), |id, mut row| {
-            let first_ts = packets.get(&0).unwrap().timestamp;
-            let packet = packets.get(&id).unwrap();
-            row.col(|ui| {
-                ui.label(id.to_string());
-            });
-            let timestamp = packet.timestamp - first_ts;
-            row.col(|ui| {
-                ui.label(timestamp.as_secs_f64().to_string());
-            });
-            row.col(|ui| {
-                ui.label(packet.source_addr.to_string());
-            });
-            row.col(|ui| {
-                ui.label(packet.destination_addr.to_string());
-            });
-            row.col(|ui| {
-                ui.label(packet.transport_protocol.to_string());
-            });
-            row.col(|ui| {
-                ui.label(packet.length.to_string());
-            });
-            let (_, resp) = row.col(|ui| {
-                ui.label(packet.session_protocol.to_string());
-            });
+        let rtp_packets: Vec<_> = packets
+            .values()
+            .filter(|packet| packet.session_protocol == Rtp)
+            .collect();
 
-            resp.context_menu(|ui| {
-                if let Some(req) = self.build_parse_menu(ui, packet) {
-                    requests.push(req);
+        rtp_packets.iter().for_each(|packet| {
+            let SessionPacket::Rtp(ref rtp_packet) = packet.contents else {
+                unreachable!();
+            };
+            set.insert(rtp_packet.ssrc);
+        });
+
+        let mut ssrcs: Vec<_> = set.into_iter().collect();
+        ssrcs.sort();
+
+
+        body.rows(25.0, ssrcs.len(), |id, mut row| {
+            let stream_ssrc = ssrcs.get(id).unwrap();
+            let stream_packets: Vec<_> = rtp_packets
+                .iter()
+                .filter(|packet| {
+                    let SessionPacket::Rtp(ref rtp_packet) = packet.contents else {
+                        unreachable!();
+                    };
+                    rtp_packet.ssrc == *stream_ssrc
+                })
+                .collect();
+
+            row.col(|ui| {
+                ui.label(stream_ssrc.to_string());
+            });
+            row.col(|ui| {
+                ui.label(stream_packets.first().unwrap().source_addr.to_string());
+            });
+            row.col(|ui| {
+                ui.label(stream_packets.first().unwrap().destination_addr.to_string());
+            });
+            row.col(|ui| {
+                ui.label(stream_packets.len().to_string());
+            });
+            row.col(|ui| {
+                let duration = stream_packets.last().unwrap().timestamp.checked_sub(
+                    stream_packets.first().unwrap().timestamp);
+
+                if let Some(dur) = duration {
+                    ui.label(format!("{:?}", dur));
+                };
+            });
+            row.col(|ui| {
+                let number_of_packets = stream_packets.len() as f64;
+                if number_of_packets == 0.0 {
+                    ui.label("0.00%");
+                    return;
                 }
+
+                let first_packet = stream_packets.first().unwrap();
+                let last_packet = stream_packets.last().unwrap();
+                let SessionPacket::Rtp(ref first_rtp) = first_packet.contents else {
+                    unreachable!();
+                };
+                let SessionPacket::Rtp(ref last_rtp) = last_packet.contents else {
+                    unreachable!();
+                };
+
+                let first_sequence_number = first_rtp.sequence_number as f64;
+                let last_sequence_number = last_rtp.sequence_number as f64;
+                let expected_number_of_packets = last_sequence_number - first_sequence_number + 1.0;
+                let lost_percentage = 100.0 - (number_of_packets / expected_number_of_packets) * 100.0;
+                ui.label(format!("{:.2}%", lost_percentage));
+            });
+            row.col(|ui| {
+                let mut jitter = 0.0;
+
+                let mut last_packet: Option<&&&Packet> = None;
+                stream_packets.iter().for_each(|packet| {
+                    if let Some(prev_packet) = last_packet {
+                        let SessionPacket::Rtp(ref rtp) = packet.contents else {
+                            unreachable!();
+                        };
+                        let SessionPacket::Rtp(ref last_rtp) = prev_packet.contents else {
+                            unreachable!();
+                        };
+
+
+                        if rtp.payload_type.clock_rate.is_none() || rtp.payload_type.id != last_rtp.payload_type.id {
+                            jitter = 0.0;
+                        } else {
+                            let clock_rate = rtp.payload_type.clock_rate.unwrap();
+                            let unit_timestamp = 1.0 / clock_rate as f64;
+                            let arrival_time_difference_result = packet.timestamp.checked_sub(prev_packet.timestamp);
+                            if let Some(arrival_time_difference) = arrival_time_difference_result {
+                                let timestamp_difference = rtp.timestamp as f64 * unit_timestamp
+                                    - last_rtp.timestamp as f64 * unit_timestamp;
+                                let d = arrival_time_difference.as_secs_f64() - timestamp_difference;
+
+                                jitter = jitter + (d - jitter) / 16.0;
+                            }
+                        }
+                        last_packet = Some(packet)
+                    } else {
+                        last_packet = Some(packet);
+                    }
+                });
+
+                ui.label(jitter.to_string());
             });
         });
     }
 
-    fn build_parse_menu(&self, ui: &mut egui::Ui, packet: &Packet) -> Option<Request> {
-        let mut request = None;
-        ui.label(format!("Parse {} as:", &packet.id));
-        SessionProtocol::all().iter().for_each(|packet_type| {
-            let is_type = packet.session_protocol == *packet_type;
-            if ui.radio(is_type, packet_type.to_string()).clicked() {
-                request = Some(Request::Reparse(packet.id, *packet_type));
-            }
-        });
-        ui.separator();
-        ui.label("This will have effect on every client!");
-
-        request
-    }
+    // fn calculate_jitter(&mut self, last_jitter: f64, last_packet: &Packet, new_packet: &Packet) -> f64 {
+    //     if let Some(clock_rate) = last_packet.payload_type.clock_rate_in_hz {
+    //         if last_packet.packet.header.payload_type != new_packet.packet.header.payload_type {
+    //             return 0.0;
+    //         }
+    //
+    //         let unit_timestamp = 1.0 / clock_rate as f64;
+    //
+    //         let arrival_time_difference_result = new_packet.timestamp.checked_sub(last_packet.timestamp);
+    //
+    //         if let Some(arrival_time_difference) = arrival_time_difference_result {
+    //             let timestamp_difference = new_packet.packet.header.timestamp as f64 * unit_timestamp
+    //                 - last_packet.packet.header.timestamp as f64 * unit_timestamp;
+    //             let d = arrival_time_difference.as_secs_f64() - timestamp_difference;
+    //             return last_jitter + (d - last_jitter) / 16.0;
+    //         }
+    //         // self.jitter_history
+    //         //     .push((self.jitter, arrival_timestamp.as_secs_f64()));
+    //         0.0
+    //     } else {
+    //         0.0
+    //     }
+    // }
 }
