7d7e7ae76cf0f7ee661ebf1b79a8aa2113bc1cb8
Łukasz Wala <84684231+LVala@users.noreply.github.com>
2023-09-15 10:02:22 +0200

    Fix packet recognition issues (#40)

--- common/src/rtcp.rs
+++ common/src/rtcp.rs
@@ -12,57 +12,65 @@ pub enum RtcpPacket {
     ReceiverReport(receiver_report::ReceiverReport),
     SourceDescription(source_description::SourceDescription),
     Goodbye(goodbye::Goodbye),
+    ApplicationDefined,
+    PayloadSpecificFeedback,
+    TransportSpecificFeedback,
+    ExtendedReport,
+    Other,
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 impl RtcpPacket {
     pub fn build(packet: &super::Packet) -> Option<Vec<Self>> {
         use rtcp::packet;
+
         // payload field should never be empty
         // except for when encoding the packet
         let mut buffer: &[u8] = packet
             .payload
             .as_ref()
             .expect("Packet's payload field is empty");
+
         let Ok(rtcp_packets) = packet::unmarshal(&mut buffer) else {
             return None;
         };
 
         let packets: Vec<_> = rtcp_packets
             .into_iter()
-            .filter_map(|packet| Self::cast_to_packet(packet))
+            .map(|(packet, packet_type)| Self::cast_to_packet(packet, packet_type))
             .collect();
 
         Some(packets)
     }
 
-    fn cast_to_packet(packet: Box<dyn rtcp::packet::Packet>) -> Option<Self> {
-        use rtcp::goodbye::Goodbye;
-        use rtcp::receiver_report::ReceiverReport;
-        use rtcp::sender_report::SenderReport;
-        use rtcp::source_description::SourceDescription;
+    fn cast_to_packet(
+        packet: Box<dyn rtcp::packet::Packet>,
+        packet_type: rtcp::header::PacketType,
+    ) -> Self {
+        use rtcp::header::PacketType;
 
-        let packet = packet.as_any();
-
-        if let Some(pack) = packet.downcast_ref::<SenderReport>() {
-            return Some(RtcpPacket::SenderReport(sender_report::SenderReport::new(
-                pack,
-            )));
+        match packet_type {
+            PacketType::SenderReport => {
+                let pack = packet.as_any().downcast_ref().unwrap();
+                RtcpPacket::SenderReport(sender_report::SenderReport::new(pack))
+            }
+            PacketType::ReceiverReport => {
+                let pack = packet.as_any().downcast_ref().unwrap();
+                RtcpPacket::ReceiverReport(receiver_report::ReceiverReport::new(pack))
+            }
+            PacketType::SourceDescription => {
+                let pack = packet.as_any().downcast_ref().unwrap();
+                RtcpPacket::SourceDescription(source_description::SourceDescription::new(pack))
+            }
+            PacketType::Goodbye => {
+                let pack = packet.as_any().downcast_ref().unwrap();
+                RtcpPacket::Goodbye(goodbye::Goodbye::new(pack))
+            }
+            PacketType::ApplicationDefined => RtcpPacket::ApplicationDefined,
+            PacketType::PayloadSpecificFeedback => RtcpPacket::PayloadSpecificFeedback,
+            PacketType::TransportSpecificFeedback => RtcpPacket::TransportSpecificFeedback,
+            PacketType::ExtendedReport => RtcpPacket::ExtendedReport,
+            PacketType::Unsupported => RtcpPacket::Other,
         }
-        if let Some(pack) = packet.downcast_ref::<ReceiverReport>() {
-            return Some(RtcpPacket::ReceiverReport(
-                receiver_report::ReceiverReport::new(pack),
-            ));
-        }
-        if let Some(pack) = packet.downcast_ref::<SourceDescription>() {
-            return Some(RtcpPacket::SourceDescription(
-                source_description::SourceDescription::new(pack),
-            ));
-        }
-        if let Some(pack) = packet.downcast_ref::<Goodbye>() {
-            return Some(RtcpPacket::Goodbye(goodbye::Goodbye::new(pack)));
-        }
-
-        None
     }
 }
