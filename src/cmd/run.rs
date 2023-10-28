fa64726d7f386824982a4510cab0f2d75264202c
Damian Tworek <64365037+Falon452@users.noreply.github.com>
2023-10-28 22:44:59 +0200

    Allow input change from user (#46)
    Co-authored-by: Łukasz Wala <lukaszpwala@gmail.com>

--- src/cmd/run.rs
+++ src/cmd/run.rs
@@ -1,59 +1,58 @@
 use crate::server;
-use crate::sniffer::Sniffer;
-use clap::Args;
-use std::net::SocketAddr;
+use crate::sniffer::{Error, Sniffer};
+use std::collections::HashMap;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-const DEFAULT_PORT: &str = "3550";
-const DEFAULT_IP: &str = "0.0.0.0";
+const DEFAULT_PORT: u16 = 3550;
+const DEFAULT_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 
-#[derive(Debug, Args)]
+#[derive(Debug, clap::Args)]
 pub struct Run {
-    /// Network interface name or file path
-    input: String,
-    /// If set, input argument will be treated as a file path, not interface name
-    #[arg(short, long, default_value_t = false)]
-    file: bool,
-    /// IP address, if not specified, 0.0.0.0 is used
-    #[arg(short, long)]
-    address: Option<String>,
-    /// port, if not specified, 3550 is used
-    #[arg(short, long)]
-    port: Option<String>,
+    /// Pcap files to capture the packets from
+    #[arg(short, long, num_args = 1..)]
+    files: Vec<String>,
+    /// Network interfaces to capture the packets from
+    #[arg(short, long, num_args = 1..)]
+    interfaces: Vec<String>,
+    /// IP address used by the application
+    #[arg(short, long, default_value_t = DEFAULT_IP)]
+    address: IpAddr,
+    /// Port used by the application
+    #[arg(short, long, default_value_t = DEFAULT_PORT)]
+    port: u16,
 }
 
 impl Run {
     pub async fn run(self) {
-        let ip = self.address.unwrap_or(DEFAULT_IP.to_string());
-        let port = self.port.unwrap_or(DEFAULT_PORT.to_string());
-        let address = format!("{ip}:{port}");
-
-        let Ok(address) = address.parse() else {
-            println!("Error: IP address or port are invalid");
+        if self.files.is_empty() && self.interfaces.is_empty() {
+            // TODO: use some pretty printing (colors, bold font etc.)
+            println!("Error: no pcap files or network interfaces were passed");
             return;
-        };
-
-        if self.file {
-            analyze_file(self.input, address).await
-        } else {
-            capture_packets(self.input, address).await
         }
+
+        let file_sniffers = get_sniffers(self.files, Sniffer::from_file);
+        let interface_sniffers = get_sniffers(self.interfaces, Sniffer::from_device);
+        let address = SocketAddr::new(self.address, self.port);
+
+        server::run(interface_sniffers, file_sniffers, address).await;
     }
 }
 
-async fn analyze_file(file: String, address: SocketAddr) {
-    let Ok(sniffer) = Sniffer::from_file(&file) else {
-        println!("Error:cannot open the file");
-        return;
-    };
-
-    server::run(sniffer, address).await;
-}
-
-async fn capture_packets(interface: String, address: SocketAddr) {
-    let Ok(sniffer) = Sniffer::from_device(&interface) else {
-        println!("Error: cannot open the network interface");
-        return;
-    };
-
-    server::run(sniffer, address).await;
+fn get_sniffers<T, F>(mut sources: Vec<String>, get_sniffer: F) -> HashMap<String, Sniffer<T>>
+where
+    T: pcap::Activated,
+    F: Fn(&str) -> Result<Sniffer<T>, Error>,
+{
+    sources.sort_unstable();
+    sources.dedup();
+    sources
+        .into_iter()
+        .filter_map(|source| match get_sniffer(&source) {
+            Ok(sniffer) => Some((source, sniffer)),
+            Err(_) => {
+                println!("Failed to capture packets from source {}", source);
+                None
+            }
+        })
+        .collect()
 }
