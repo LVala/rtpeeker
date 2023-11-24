edd44f6e191451571b4372344ac138fced7f2338
damian-tworek <damian.tworek@spoton.com>
2023-11-25 00:43:01 +0100

    Revert changes in goodbye

--- common/src/rtcp/goodbye.rs
+++ common/src/rtcp/goodbye.rs
@@ -1,25 +1,19 @@
-use std::time::Duration;
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Goodbye {
     pub sources: Vec<u32>,
     pub reason: String,
-    pub timestamp: Duration,
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 impl Goodbye {
-    pub fn new(goodbye: &rtcp::goodbye::Goodbye, packet: &Packet) -> Self {
-        let reason = std::str::from_utf8(&goodbye.reason[..])
-            .unwrap()
-            .to_string();
+    pub fn new(packet: &rtcp::goodbye::Goodbye) -> Self {
+        let reason = std::str::from_utf8(&packet.reason[..]).unwrap().to_string();
 
         Self {
-            sources: goodbye.sources.clone(),
+            sources: packet.sources.clone(),
             reason,
-            timestamp: packet.timestamp,
         }
     }
 }
